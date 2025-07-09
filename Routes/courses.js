445ab793a5f7f48e6506956bd4e850884131a8ab
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-07-09 19:08:30 +0530

    made some changes in routes

--- Routes/courses.js
+++ Routes/courses.js
@@ -2,11 +2,15 @@ const { Router} = require("express");
 const courseRouter = Router();
 
 courseRouter.get('/preview',function(req, res){
-    
+     res.json({
+    message : "all the courses are visible here"
+})
 })
 
 courseRouter.post('/purchase', function(req, res){
-
+ res.json({
+    message : "make the payment"
+})
 })
 
 module.exports = {
