7c8940a7977a5d7f32ad563c891e570864e3eafa
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-07-20 16:33:05 +0530

    Fixed the CORS issue and Added Server running message in the Backend

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+
 const { userRouter} = require("./Routes/user");
 const { courseRouter} = require("./Routes/courses");
+
+const app = express();
+
+// This will handle the CORS Error.
+app.use(cors());
+app.use(express.json());
+
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 
-
-
-
-app.listen(3000);
+//Indicates that server is runnign on PORT 3000.
+app.listen(3000, () => {
+    console.log("Server is running on PORT 3000");
+});
